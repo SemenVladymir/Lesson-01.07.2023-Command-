9526536c89835fba0076bb5f959b590222ab3a96
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2023-07-01 10:07:36 +0300

    Добавьте файлы проекта.

---
+++ Model/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_01._07._2023__Command_.Model
+{
+    internal class User : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int id;
+        private string name;
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; OnPropertyChanged("Email"); }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("Name"); }
+        }
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); }
+        }
+
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}) {Name}\t{Email}";
+        }
+    }
+}
---
+++ ViewModel/MainVM.cs
@@ -0,0 +1,76 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Lesson_01._07._2023__Command_.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_01._07._2023__Command_.ViewModel
+{
+    internal class MainVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<User> Users { get; set; }
+        private User selectedUser;
+
+        public User MySelectedUser
+        {
+            get { return selectedUser; }
+            set { selectedUser = value; OnPropertyChanged("MySelectedUser"); }
+        }
+
+
+        private RelayCommand addCommand;
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                if (addCommand == null)
+                    addCommand = new RelayCommand(AddTestUser);
+
+                return addCommand;
+            }
+        }
+
+        private RelayCommand deleteCommand;
+
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                if (deleteCommand == null)
+                    deleteCommand = new RelayCommand(DeleteUser);
+
+                return deleteCommand;
+            }
+        }
+
+
+        public MainVM()
+        {
+            Users = new ObservableCollection<User>()
+            {
+                new User() {Id = 1, Email = "vasil@yandex.ru", Name = "Artem"},
+                new User() {Id = 2, Email = "bigbebrik@gmail.com", Name = "Kolya" }
+            };
+            MySelectedUser = Users[0];
+        }
+
+        public void AddTestUser() => Users.Add(new User() { Id = Users.LastOrDefault().Id + 1, Email = "test@mail.mu", Name = "Test" });
+
+        public void DeleteUser() => Users.Remove(selectedUser);
+
+        //public void ParamTest(object obj) => Users.Add(new User() { Id = Users.LastOrDefault().Id + 1, Email = "test@mail.mu", Name = "Test" });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
